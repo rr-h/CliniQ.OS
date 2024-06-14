421eef964f7dfe7c199bf5f8fb470ca921fc1153
rr-h <reuther.hols@gmail.com>
2024-06-14 06:51:43 +0000

    14 Jun 2024, 07:51

---
+++ backend/controllers/data_controller.js
@@ -0,0 +1,10 @@
+import dataModel from '../models/dataModel.js';
+
+export const getData = async (req, res) => {
+  try {
+    const data = await dataModel.getData();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+};
