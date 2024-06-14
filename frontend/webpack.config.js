369fa5b0b17188fdb13f8fcfcb1a749540887528
rr-h <reuther.hols@gmail.com>
2024-06-14 10:29:30 +0000

    14 Jun 2024, 11:29

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './frontend/main.js',
+  entry: './main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
