88c8c06f1c937b164ccb54bd5a42beaf4fcb6a3c
rr-h <reuther.hols@gmail.com>
2024-06-16 02:25:15 +0100

    update 7 files, delete 1 file, create 11 files, rename 9 files and move and rename 6 files

---
+++ frontend/CliniQ.OS/js/randomFonts.js
@@ -0,0 +1,26 @@
+// randomFonts.js
+
+// List of font families
+const fonts = [
+   'ArgentPixelCF-Italic', 'ArgentPixelCF-Regular', 'Charcoal',
+   'ChicagoFLF', 'chikarego2-webfont', 'Gelion Black Italic',
+   'Gelion Black', 'Gelion Bold Italic', 'Gelion Bold',
+   'Gelion Light Italic', 'Gelion Light', 'Gelion Medium Italic',
+   'Gelion Medium', 'Gelion Regular Italic', 'Gelion',
+   'Gelion SemiBold Italic', 'Gelion SemiBold', 'Gelion Thin Italic',
+   'Gelion Thin', 'geneva_9', 'Inconsolata-Bold', 'Inconsolata-Regular',
+   'Inconsolata', 'PerfectDOSVGA437', 'PixelArial11', 'pixelmix_bold',
+   'pixelmix', 'Pixolde-Bold', 'Pixolde'
+];
+
+// Function to apply a random font family to each element
+function applyRandomFontsToElements() {
+   const elements = document.querySelectorAll('*');
+   elements.forEach(element => {
+      const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+      element.style.fontFamily = randomFont;
+   });
+}
+
+// Apply random fonts on page load
+window.onload = applyRandomFontsToElements;
--- frontend/main.js
+++ frontend/main.js
@@ -5,7 +5,7 @@ import { handleSubmit, handleClick } from './components/EventHandler.js';
 import './CliniQ.OS/css/styles.css';
 import './CliniQ.OS/css/clinic_OS.min.css';
 import './CliniQ.OS/css/fonts.css';
-import './CliniQ.OS/js/clinic.OS.webflow.js';
+import './CliniQ.OS/js/cliniq_OS.webflow.js';
 import './CliniQ.OS/js/jquery-3.3.1.min.js';
 import './CliniQ.OS/js/jquery-3.5.1.min.dc5e7f18c8.js';
 import './CliniQ.OS/js/jquery-ui.min.js';
