c30806a27d8b2ef9b1ebb708574e04b3d89ae7d3
rr-h <reuther.hols@gmail.com>
2024-06-16 11:05:57 +0000

    16 Jun 2024, 12:05

--- frontend/main.js
+++ frontend/main.js
@@ -1,28 +1,24 @@
+// js/main.js
 import { handleSubmit, handleClick } from './components/EventHandler.js';
 import { fetchData, processData } from './utils/dataHandlers.js';
 import { displayData } from './components/UIComponents.js';
-import './CliniQ.OS/css/styles.css';
-import './CliniQ.OS/css/clinic_OS.min.css';
-import './CliniQ.OS/css/fonts.css';
-import './CliniQ.OS/js/cliniq_OS.webflow.js';
-import './CliniQ.OS/js/jquery-3.3.1.min.js';
-import './CliniQ.OS/js/jquery-3.5.1.min.dc5e7f18c8.js';
-import './CliniQ.OS/js/jquery-ui.min.js';
-import './CliniQ.OS/js/jquery.validate.min.js';
-import './CliniQ.OS/js/select.menu.custom.js';
-import './CliniQ.OS/js/widget.js';
+import './CliniQ.OS_UI/css/styles.css';
+import './CliniQ.OS_UI/css/clinic_OS.min.css';
+import './CliniQ.OS_UI/css/fonts.css';
+import './CliniQ.OS_UI/js/cliniq_OS.webflow.js';
+import './CliniQ.OS_UI/js/jquery-3.3.1.min.js';
+import './CliniQ.OS_UI/js/jquery-3.5.1.min.dc5e7f18c8.js';
+import './CliniQ.OS_UI/js/jquery-ui.min.js';
+import './CliniQ.OS_UI/js/jquery.validate.min.js';
+import './CliniQ.OS_UI/js/select.menu.custom.js';
+import './CliniQ.OS_UI/js/widget.js';
 
-
-
-document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    const data = await fetchData('/api/data');
+// Initialize event listeners
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#form').addEventListener('submit', handleSubmit);
+  document.querySelector('#button').addEventListener('click', handleClick);
+  fetchData().then(data => {
     const processedData = processData(data);
     displayData(processedData);
-  } catch (error) {
-    console.error(error);
-  }
+  });
 });
-
-document.addEventListener('submit', handleSubmit);
-document.addEventListener('click', handleClick);
