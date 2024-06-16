14f367e7befb2d773a3f300d7ff08978ab1e559e
rr-h <reuther.hols@gmail.com>
2024-06-16 14:28:36 +0000

    16 Jun 2024, 15:28

--- frontend/main.js
+++ frontend/main.js
@@ -4,16 +4,16 @@ import { initUIComponents } from './components/UIComponents';
 import { handleSubmit, handleClick } from './components/EventHandler.js';
 import { fetchData, processData } from './utils/dataHandlers.js';
 import { displayData } from './components/UIComponents.js';
-import './iQ.OS_UI/css/styles.css';
-import './CliniQ.OS_UI/css/cliniq_OS.min.css';
-import './CliniQ.OS_UI/css/fonts.css';
-import './CliniQ.OS_UI/js/cliniq_OS.webflow.js';
-import './CliniQ.OS_UI/js/jquery-3.3.1.min.js';
-import './CliniQ.OS_UI/js/jquery-3.5.1.min.dc5e7f18c8.js';
-import './CliniQ.OS_UI/js/jquery-ui.min.js';
-import './CliniQ.OS_UI/js/jquery.validate.min.js';
-import './CliniQ.OS_UI/js/select.menu.custom.js';
-import './CliniQ.OS_UI/js/widget.js';
+import './CQ.OS_UI/css/styles.css';
+import './CQ.QS_UI/css/cliniq_OS.min.css';
+import './CQ.QS_UI/css/fonts.css';
+import './CQ.QS_UI/js/cliniq_OS.webflow.js';
+import './CQ.QS_UI/js/jquery-3.3.1.min.js';
+import './CQ.QS_UI/js/jquery-3.5.1.min.dc5e7f18c8.js';
+import './CQ.QS_UI/js/jquery-ui.min.js';
+import './CQ.QS_UI/js/jquery.validate.min.js';
+import './CQ.QS_UI/js/select.menu.custom.js';
+import './CQ.QS_UI/js/widget.js';
 
 // Initialize UI components
 initUIComponents();
