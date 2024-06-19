893133d4bbe414bc0e968d008e9c758c716a2fc4
rr-h <reuther.hols@gmail.com>
2024-06-19 01:29:06 +0000

    19 Jun 2024, 02:29

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
+// logger.js
 const loggerMiddleware = (req, res, next) => {
-  module.exports = (req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
-  }
-}
+};
+
 export default loggerMiddleware;
