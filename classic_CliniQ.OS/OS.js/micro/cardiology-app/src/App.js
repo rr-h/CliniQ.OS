ab3ec692928325f816549afbdbbb27683044386d
rr-h <reuther.hols@gmail.com>
2024-06-24 13:51:06 +0000

    24 Jun 2024, 14:51

---
+++ classic_CliniQ.OS/OS.js/micro/cardiology-app/src/App.js
@@ -0,0 +1,12 @@
+import CardiologyComponent from './components/CardiologyComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CardiologyComponent />
+    </div>
+  );
+}
+
+export default App;
