920d71a4559c458524643ad4a295c8dfd11b02a0
rr-h <reuther.hols@gmail.com>
2024-06-24 12:33:10 +0000

    24 Jun 2024, 13:33

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,41 @@
 const config = {
-  extends: [
-    'kentcdodds',
-    'kentcdodds/jest',
-    'plugin:node-dependencies/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2021
-  },
-  rules: {
-    'valid-jsdoc': 'off',
-    'max-len': 'off',
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'never',
-        named: 'never',
-        asyncArrow: 'always'
-      }
+    extends: [
+        'kentcdodds',
+        'kentcdodds/jest',
+        'plugin:node-dependencies/recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:react-hooks/recommended'
     ],
-    'import/no-import-module-exports': 'off',
-    'arrow-parens': ['error', 'as-needed'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-  },
-  settings: {
-    'import/ignore': ['node_modules', 'src']
-  }
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    rules: {
+        'valid-jsdoc': 'off',
+        'max-len': 'off',
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'never',
+                named: 'never',
+                asyncArrow: 'always'
+            }
+        ],
+        'import/no-import-module-exports': 'off',
+        'arrow-parens': ['error', 'as-needed'],
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'react/prop-types': 'off', // Assuming you use TypeScript for type checking
+    },
+    settings: {
+        'import/ignore': ['node_modules', 'src'],
+        react: {
+            version: 'detect'
+        }
+    }
 };
 
 module.exports = config;
