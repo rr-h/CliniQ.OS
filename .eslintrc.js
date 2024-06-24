c2ca70bb7833a95a9ed763d5022a29db605c2fa4
rr-h <reuther.hols@gmail.com>
2024-06-24 12:23:52 +0000

    24 Jun 2024, 13:23

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const config = {
+  extends: [
+    'kentcdodds',
+    'kentcdodds/jest',
+    'plugin:node-dependencies/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021
+  },
+  rules: {
+    'valid-jsdoc': 'off',
+    'max-len': 'off',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'import/no-import-module-exports': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+  },
+  settings: {
+    'import/ignore': ['node_modules', 'src']
+  }
+};
+
+module.exports = config;
