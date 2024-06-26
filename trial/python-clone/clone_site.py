c21b414b9e7ce3c9d1bca995f21ea2babc0aaa36
rr-h <reuther.hols@gmail.com>
2024-06-26 12:45:16 +0000

    26 Jun 2024, 13:45

--- trial/python-clone/clone_site.py
+++ trial/python-clone/clone_site.py
@@ -36,7 +36,6 @@ def parse_ssg_manifest(file_path):
     try:
         with open(file_path, 'r') as f:
             content = f.read()
-            # Updated regular expression to match the JSON structure
             match = re.search(r"self\.__SSG_MANIFEST\s*=\s*new\s+Set\((\[[\s\S]*?\])\);", content)
             if match:
                 json_str = match.group(1)
