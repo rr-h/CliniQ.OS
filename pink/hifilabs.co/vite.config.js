ed1c8fce0b5300f645e7f217b35ee1879cd4537d
rr-h <reuther.hols@gmail.com>
2024-06-26 01:10:44 +0000

    26 Jun 2024, 02:10

--- pink/hifilabs.co/vite.config.js
+++ pink/hifilabs.co/vite.config.js
@@ -1,24 +1,26 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '.',
-    build: {
-        rollupOptions: {
-            input: './index.html',
-            external: [
-                '/_next/static/chunks/polyfills-c67a75d1b6f99dc8.js',
-                '/_next/static/chunks/webpack-fd418f31fe4b0d08.js',
-                '/_next/static/chunks/framework-0ba0ddd33199226d.js',
-                '/_next/static/chunks/main-c0364ec597719817.js',
-                '/_next/static/chunks/pages/_app-ccef8dd21efcc9f9.js',
-                '/_next/static/chunks/837-c19bc76e77d41ef3.js',
-                '/_next/static/chunks/984-656420980a2b2a72.js',
-                '/_next/static/chunks/803-7cd30887c5daaa8a.js',
-                '/_next/static/chunks/739-32fc889ec63fd09b.js',
-                '/_next/static/chunks/pages/[windowId]-b1c419fdaf934173.js',
-                '/_next/static/_qbKLC8NwpL61siZyGWi2/_buildManifest.js',
-                '/_next/static/_qbKLC8NwpL61siZyGWi2/_ssgManifest.js'
-            ]
-        }
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: './index.html',
+      external: [
+        '/js/polyfills-c67a75d1b6f99dc8.js',
+        '/js/webpack-fd418f31fe4b0d08.js',
+        '/js/framework-0ba0ddd33199226d.js',
+        '/js/main-c0364ec597719817.js',
+        '/js/_app-ccef8dd21efcc9f9.js',
+        '/js/837-c19bc76e77d41ef3.js',
+        '/js/984-656420980a2b2a72.js',
+        '/js/803-7cd30887c5daaa8a.js',
+        '/js/739-32fc889ec63fd09b.js',
+        '/js/[windowId]-b1c419fdaf934173.js',
+        '/js/_buildManifest.js',
+        '/js/_ssgManifest.js',
+        '/css/75fb90901c2ee1f9.css'
+      ]
     }
+  },
+  publicDir: './public'
 });
