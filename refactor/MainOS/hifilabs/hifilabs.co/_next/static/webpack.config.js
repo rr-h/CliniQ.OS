72b8b0d722b37b30a3fa465c3cfa0a881edad5e9
rr-h <reuther.hols@gmail.com>
2024-06-27 16:49:58 +0000

    27 Jun 2024, 17:49

---
+++ refactor/MainOS/hifilabs/hifilabs.co/_next/static/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
+  }
+};
