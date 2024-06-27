b0b67fcfcae685d6e57a0f6db3b2115c85ec6d8b
rr-h <reuther.hols@gmail.com>
2024-06-27 23:33:44 +0000

    28 Jun 2024, 00:33

--- FINAL/hifilabs_mirror/hifilabs.co/_next/static/_qbKLC8NwpL61siZyGWi2/_buildManifest.js
+++ FINAL/hifilabs_mirror/hifilabs.co/_next/static/_qbKLC8NwpL61siZyGWi2/_buildManifest.js
@@ -1,1 +1,56 @@
-self.__BUILD_MANIFEST=function(s,o,i,l){return{__rewrites:{beforeFiles:[],afterFiles:[{source:"/hifi/js/info.js"},{source:"/hifi/js/info.exclusions.js"},{source:"/hifi/js/info.exclusions.local.js"},{source:"/hifi/js/info.exclusions.local.manual.js"},{source:"/hifi/js/info.exclusions.local.manual.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.local.manual.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.local.manual.js"},{source:"/hifi/js/info.exclusions.local.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.local.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.local.js"},{source:"/hifi/js/info.exclusions.manual.js"},{source:"/hifi/js/info.exclusions.manual.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.manual.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.manual.js"},{source:"/hifi/js/info.exclusions.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.outbound-links.js"},{source:"/hifi/js/info.exclusions.file-downloads.js"},{source:"/hifi/js/info.local.js"},{source:"/hifi/js/info.local.manual.js"},{source:"/hifi/js/info.local.manual.outbound-links.js"},{source:"/hifi/js/info.file-downloads.local.manual.outbound-links.js"},{source:"/hifi/js/info.file-downloads.local.manual.js"},{source:"/hifi/js/info.local.outbound-links.js"},{source:"/hifi/js/info.file-downloads.local.outbound-links.js"},{source:"/hifi/js/info.file-downloads.local.js"},{source:"/hifi/js/info.manual.js"},{source:"/hifi/js/info.manual.outbound-links.js"},{source:"/hifi/js/info.file-downloads.manual.outbound-links.js"},{source:"/hifi/js/info.file-downloads.manual.js"},{source:"/hifi/js/info.outbound-links.js"},{source:"/hifi/js/info.file-downloads.outbound-links.js"},{source:"/hifi/js/info.file-downloads.js"},{source:"/hifi/api/event"}],fallback:[]},"/":[s,o,i,l,"static/chunks/pages/index-453bea2c42b4bda8.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],"/artistlab/[slug]":[s,o,i,l,"static/chunks/pages/artistlab/[slug]-cf62f73bbcd2c719.js"],"/idealab/[slug]":[s,o,i,l,"static/chunks/pages/idealab/[slug]-58d4fcc08a1687c0.js"],"/web3cohort/[slug]":[s,o,i,l,"static/chunks/pages/web3cohort/[slug]-d4e0df7604989401.js"],"/[windowId]":[s,o,i,l,"static/chunks/pages/[windowId]-b1c419fdaf934173.js"],sortedPages:["/","/_app","/_error","/artistlab/[slug]","/idealab/[slug]","/web3cohort/[slug]","/[windowId]"]}}("static/chunks/837-c19bc76e77d41ef3.js","static/chunks/984-656420980a2b2a72.js","static/chunks/803-7cd30887c5daaa8a.js","static/chunks/739-32fc889ec63fd09b.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
+(self.__BUILD_MANIFEST = (function(s, o, i, l) {
+  return {
+    __rewrites: {
+      beforeFiles: [],
+      afterFiles: [
+        { source: '/hifi/js/info.js' },
+        { source: '/hifi/js/info.exclusions.js' },
+        { source: '/hifi/js/info.exclusions.local.js' },
+        { source: '/hifi/js/info.exclusions.local.manual.js' },
+        { source: '/hifi/js/info.exclusions.local.manual.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.local.manual.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.local.manual.js' },
+        { source: '/hifi/js/info.exclusions.local.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.local.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.local.js' },
+        { source: '/hifi/js/info.exclusions.manual.js' },
+        { source: '/hifi/js/info.exclusions.manual.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.manual.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.manual.js' },
+        { source: '/hifi/js/info.exclusions.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.outbound-links.js' },
+        { source: '/hifi/js/info.exclusions.file-downloads.js' },
+        { source: '/hifi/js/info.local.js' },
+        { source: '/hifi/js/info.local.manual.js' },
+        { source: '/hifi/js/info.local.manual.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.local.manual.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.local.manual.js' },
+        { source: '/hifi/js/info.local.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.local.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.local.js' },
+        { source: '/hifi/js/info.manual.js' },
+        { source: '/hifi/js/info.manual.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.manual.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.manual.js' },
+        { source: '/hifi/js/info.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.outbound-links.js' },
+        { source: '/hifi/js/info.file-downloads.js' },
+        { source: '/hifi/api/event' }
+      ],
+      fallback: []
+    },
+    '/': [s, o, i, l, 'static/chunks/pages/index-453bea2c42b4bda8.js'],
+    '/_error': ['static/chunks/pages/_error-effe22be6ff34abe.js'],
+    '/artistlab/[slug]': [s, o, i, l, 'static/chunks/pages/artistlab/[slug]-cf62f73bbcd2c719.js'],
+    '/idealab/[slug]': [s, o, i, l, 'static/chunks/pages/idealab/[slug]-58d4fcc08a1687c0.js'],
+    '/web3cohort/[slug]': [s, o, i, l, 'static/chunks/pages/web3cohort/[slug]-d4e0df7604989401.js'],
+    '/[windowId]': [s, o, i, l, 'static/chunks/pages/[windowId]-b1c419fdaf934173.js'],
+    sortedPages: ['/', '/_app', '/_error', '/artistlab/[slug]', '/idealab/[slug]', '/web3cohort/[slug]', '/[windowId]']
+  };
+})(
+  'static/chunks/837-c19bc76e77d41ef3.js',
+  'static/chunks/984-656420980a2b2a72.js',
+  'static/chunks/803-7cd30887c5daaa8a.js',
+  'static/chunks/739-32fc889ec63fd09b.js'
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
