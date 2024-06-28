39295c8eaa9c0a6e06239fdba51146d2d4d2bc29
rr-h <reuther.hols@gmail.com>
2024-06-28 00:12:18 +0000

    28 Jun 2024, 01:12

--- FINAL/hifilabs.py
+++ FINAL/hifilabs.py
@@ -1,10 +1,13 @@
-import requests_html
+import requests
 from requests_html import HTMLSession
 
+def get_html_from_url(url):
+    session = HTMLSession()
+    response = session.get(url)
+    response.html.render(sleep=2, scrolldown=1)
+    return response
+
 url = 'https://hifilabs.com/'
-s = HTMLSession()
-r = s.get(url)
-
-r.html.render(sleep=2, scrolldown=1)
-
-print(r.status.code)
+html_response = get_html_from_url(url)
+status_code = html_response.status_code
+print(status_code)
