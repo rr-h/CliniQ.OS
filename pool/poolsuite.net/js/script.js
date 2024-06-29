1389b1ce705a028a346b8924dacac823d261e997
rr-h <reuther.hols@gmail.com>
2024-06-29 22:24:51 +0000

    29 Jun 2024, 23:24

--- pool/poolsuite.net/js/script.js
+++ pool/poolsuite.net/js/script.js
@@ -1,54 +1,86 @@
-!(function () {
-  'use strict';
-  var a = window.location,
-    r = window.document,
-    o = r.currentScript,
-    l = o.getAttribute('data-api') || new URL(o.src).origin + '/api/event';
+(function() {
+    'use strict';
 
-  function s(t, e) {
-    t && console.warn('Ignoring Event: ' + t), e && e.callback && e.callback();
-  }
+    var location = window.location,
+        document = window.document,
+        currentScript = document.currentScript,
+        apiEndpoint = currentScript.getAttribute('data-api') || new URL(currentScript.src).origin + '/api/event';
 
-  function t(t, e) {
-    if (/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(a.hostname) || 'file:' === a.protocol) return s('localhost', e);
-    if ((window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress) && !window.__plausible) return s(null, e);
-    try {
-      if ('true' === window.localStorage.plausible_ignore) return s('localStorage flag', e);
-    } catch (t) {}
-    var i = {},
-      n =
-        ((i.n = t),
-        (i.u = a.href),
-        (i.d = o.getAttribute('data-domain')),
-        (i.r = r.referrer || null),
-        e && e.meta && (i.m = JSON.stringify(e.meta)),
-        e && e.props && (i.p = e.props),
-        new XMLHttpRequest());
-    n.open('POST', l, !0),
-      n.setRequestHeader('Content-Type', 'text/plain'),
-      n.send(JSON.stringify(i)),
-      (n.onreadystatechange = function () {
-        4 === n.readyState && e && e.callback && e.callback({ status: n.status });
-      });
-  }
-  var e = (window.plausible && window.plausible.q) || [];
-  window.plausible = t;
-  for (var i, n = 0; n < e.length; n++) t.apply(this, e[n]);
+    function logWarning(event, callbackObject) {
+        if (event) {
+            console.warn('Ignoring Event: ' + event);
+        }
+        if (callbackObject && callbackObject.callback) {
+            callbackObject.callback();
+        }
+    }
 
-  function p() {
-    i !== a.pathname && ((i = a.pathname), t('pageview'));
-  }
-  var c,
-    u = window.history;
-  u.pushState &&
-    ((c = u.pushState),
-    (u.pushState = function () {
-      c.apply(this, arguments), p();
-    }),
-    window.addEventListener('popstate', p)),
-    'prerender' === r.visibilityState
-      ? r.addEventListener('visibilitychange', function () {
-          i || 'visible' !== r.visibilityState || p();
-        })
-      : p();
+    function sendEvent(eventName, options) {
+        if (/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(location.hostname) || location.protocol === 'file:') {
+            return logWarning('localhost', options);
+        }
+        if ((window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress) && !window.__plausible) {
+            return logWarning(null, options);
+        }
+        try {
+            if (window.localStorage.plausible_ignore === 'true') {
+                return logWarning('localStorage flag', options);
+            }
+        } catch (err) {}
+
+        var payload = {
+            n: eventName,
+            u: location.href,
+            d: currentScript.getAttribute('data-domain'),
+            r: document.referrer || null,
+            m: options && options.meta ? JSON.stringify(options.meta) : undefined,
+            p: options && options.props ? options.props : undefined
+        };
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', apiEndpoint, true);
+        xhr.setRequestHeader('Content-Type', 'text/plain');
+        xhr.send(JSON.stringify(payload));
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && options && options.callback) {
+                options.callback({ status: xhr.status });
+            }
+        };
+    }
+
+    var initialQueue = (window.plausible && window.plausible.q) || [];
+    window.plausible = sendEvent;
+
+    for (var i = 0; i < initialQueue.length; i++) {
+        sendEvent.apply(this, initialQueue[i]);
+    }
+
+    var currentPathname;
+
+    function trackPageview() {
+        if (currentPathname !== location.pathname) {
+            currentPathname = location.pathname;
+            sendEvent('pageview');
+        }
+    }
+
+    var history = window.history;
+    if (history.pushState) {
+        var originalPushState = history.pushState;
+        history.pushState = function() {
+            originalPushState.apply(this, arguments);
+            trackPageview();
+        };
+        window.addEventListener('popstate', trackPageview);
+    }
+
+    if (document.visibilityState === 'prerender') {
+        document.addEventListener('visibilitychange', function() {
+            if (!currentPathname && document.visibilityState === 'visible') {
+                trackPageview();
+            }
+        });
+    } else {
+        trackPageview();
+    }
 })();
